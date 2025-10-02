006f2f589b68f5eadd7daec9c3005ac095b72752
cfophoangvk <hieuthien20042004@gmail.com>
2025-10-02 20:54:35 +0700

    config app

---
+++ README.md
@@ -0,0 +1,15 @@
+### HƯỚNG DẪN CHẠY
+
+1. Clone project về.
+2. Chạy `npm i`
+3. Chạy `npm start`
+
+### HƯỚNG DẪN XÂY CẤU TRÚC
+## Backend: (PORT: 6789):
+- Thêm dữ liệu các thứ vào file database.json
+- Thêm endpoint API vào server.js
+
+## Frontend: (PORT: 5173):
+- 
+
+- Nếu KHÔNG chạy được port 5173 => vào terminal gõ `netstat -ano | findstr :5173` -> dóng sang cột cuối sẽ nhìn thấy số `x` -> taskkill /PID `x` /F
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -18,29 +16,13 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <button onClick={callApi}>
-          Call API
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={() => setCount((count) => count + 1)} className='px-2 py-2 bg-slate-500 rounded-md'>
+        count is {count}
+      </button>
+      <button onClick={callApi} className='px-2 py-2 bg-slate-500 ml-3 rounded-md'>
+        Call API
+      </button>
+      <h1 className='mt-10 text-9xl text-red-700'>If you see red text, the app is working!</h1>
     </>
   )
 }
