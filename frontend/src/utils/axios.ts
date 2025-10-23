e0a0f26100d24b6267950d7950713941e5ba5b6c
cfophoangvk <hieuthien20042004@gmail.com>
2025-10-23 14:04:19 +0700

    fix url for F**KING CUSTOMER

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:6789/api/",
+  baseURL: "https://api.nourivitamin.com/api/",
   withCredentials: true, // send cookies to the server
 });
 
