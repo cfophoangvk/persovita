535666407b929de478da44cd49ce6449ea4411f2
cfophoangvk <hieuthien20042004@gmail.com>
2025-10-25 13:12:24 +0700

    fix banner isMobile

--- frontend/src/main/components/Banner.tsx
+++ frontend/src/main/components/Banner.tsx
@@ -1,10 +1,7 @@
 import React from "react";
-import { useIsMobile } from "../../common/hooks/useIsMobile";
 
 const Banner: React.FC = () => {
   const video = "/assets/banner.mp4";
-  const isMobile = useIsMobile();
-  console.log("isMobile", isMobile);
 
   return (
     <div className={`relative w-full h-[90vh] overflow-hidden md:mt-16 mt-10`}>
