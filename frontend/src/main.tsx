026a992406a67377833dba0b6cd8700ee774391f
hainvhe172343 <hainvhe172343@fpt.edu.vn>
2025-11-05 11:55:49 +0700

    fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { LoadingContextProvider } from "./common/hooks/useLoading.tsx";
+import { StrictMode } from "react";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <LoadingContextProvider>
-      <App />
-    </LoadingContextProvider>
+    <StrictMode>
+      <LoadingContextProvider>
+        <App />
+      </LoadingContextProvider>
+    </StrictMode>
   </BrowserRouter>
 );
