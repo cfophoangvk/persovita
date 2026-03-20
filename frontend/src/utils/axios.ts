ffc8bfb21f360c2157366590f095fee9a598818b
hainvhe172343 <hainvhe172343@fpt.edu.vn>
2026-03-20 17:46:41 +0700

    deploy mongodb

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://api.nourivitamin.com/api/",
+  baseURL: "https://persovita-m2y9.vercel.app/api/",
   withCredentials: true, // send cookies to the server
 });
 
